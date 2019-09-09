bfdc75af16210af712d22c39e969d8109870e7b1
CAWithey <43553630+CAWithey@users.noreply.github.com>
2019-09-09 19:49:32 -0400

    Coded Spawners
    Added spawn cubes. Updated texturing on some models.  Added floor texture and expanded play area.

---
+++ Assets/Scripts/LogicBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicBehaviour : MonoBehaviour
+{
+    public GameObject RedCube;
+    private GameObject[] Spawners;
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start()
+    {
+        GameObject spawn1 = GameObject.Find("SpawnPoint (1)");
+        Spawners = GameObject.FindGameObjectsWithTag("Spawners");
+
+        foreach (GameObject SpawnObject in Spawners)
+        {
+            SpawnObject.SetActive(!gameObject.activeSelf);
+            Instantiate(RedCube, new Vector3(SpawnObject.transform.position.x, SpawnObject.transform.position.y, SpawnObject.transform.position.z), Quaternion.identity);
+        }
+        //Spawners.SetActive(!gameObject.activeSelf);
+        print(spawn1.transform.position.x);
+        
+        //GameObject addCube;
+        //addCube = new GameObject("RedCube");
+        //addCube.AddComponent<Rigidbody>();
+        //addCube.AddComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
