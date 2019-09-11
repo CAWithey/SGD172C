a1bffec7d5e1162c0d59f596f0e1672cfe63349a
CAWithey <43553630+CAWithey@users.noreply.github.com>
2019-09-11 16:23:34 -0400

    Expanded Structural and Lighting
    Added geometry to floor plan and lighting

---
+++ Assets/Scripts/KillScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillScript : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        print("Bam!");
+    }
+}
--- Assets/Scripts/LogicBehaviour.cs
+++ Assets/Scripts/LogicBehaviour.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class LogicBehaviour : MonoBehaviour
 {
-    public GameObject RedCube;
+    public GameObject[] Spawnables;
     private GameObject[] Spawners;
 
-    private void Awake()
+    void Awake()
     {
         
     }
 
     void Start()
     {
-        GameObject spawn1 = GameObject.Find("SpawnPoint (1)");
+        //GameObject spawn1 = GameObject.Find("SpawnPoint (1)");
         Spawners = GameObject.FindGameObjectsWithTag("Spawners");
 
         foreach (GameObject SpawnObject in Spawners)
         {
             SpawnObject.SetActive(!gameObject.activeSelf);
-            Instantiate(RedCube, new Vector3(SpawnObject.transform.position.x, SpawnObject.transform.position.y, SpawnObject.transform.position.z), Quaternion.identity);
+            Instantiate(Spawnables[0], new Vector3(SpawnObject.transform.position.x, SpawnObject.transform.position.y, SpawnObject.transform.position.z), Quaternion.identity);
         }
         //Spawners.SetActive(!gameObject.activeSelf);
-        print(spawn1.transform.position.x);
+        //print(spawn1.transform.position.x);
         
         //GameObject addCube;
         //addCube = new GameObject("RedCube");
