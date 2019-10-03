8b6c1dd8378d3fa4a829de93ff6c7dda58d856db
CAWithey <43553630+CAWithey@users.noreply.github.com>
2019-10-02 20:17:17 -0400

    10.2.2019B
    Item scaling problem MOSTLY solved...

--- Assets/Scripts/HandBasketScript.cs
+++ Assets/Scripts/HandBasketScript.cs
@@ -8,11 +8,24 @@ public class HandBasketScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Foods")
         {
+            print(other.transform.parent);
+            if (other.transform.parent == null)
+            {
+                other.transform.parent = this.transform.parent;
+            }
+            print(other.transform.parent);
+            other.transform.localScale = other.transform.localScale;
+            //other.transform.parent = this.transform;
+            //ChangeParentScale(this.transform, transform.localScale);
+            //other.transform.localScale = this.transform.parent.parent.localScale;
+            //other.transform.localRotation = this.transform.parent.parent.localRotation;
+            //other.transform.localPosition = this.transform.localPosition;
             //other.transform.parent = this.transform.root;
-            other.transform.parent = this.transform;
-            other.transform.localPosition = this.transform.localPosition;
-            other.transform.localScale = other.transform.parent.parent.localScale;
-            other.transform.localRotation = other.transform.localRotation;
+            //other.transform.SetParent(transform, false);
+            //other.gameObject.transform.GetChild(0) = this.gameObject.transform;
+            //other.transform.localPosition = this.transform.localPosition;
+            //other.transform.localScale = other.transform.parent.parent.localScale;
+            //other.transform.localRotation = other.transform.localRotation;
             //var child[] = other.
             //print(this.transform.parent);
         }
@@ -21,7 +34,21 @@ public class HandBasketScript : MonoBehaviour
     {
         if (other.gameObject.tag == "Foods")
         {
+            other.transform.localScale = other.transform.localScale;
+            other.transform.localRotation = other.transform.localRotation;
             other.transform.parent = null;
         }
+    }
+
+    void ChangeParentScale(Transform parent, Vector3 scale)
+    {
+        List<Transform> children = new List<Transform>();
+        foreach (Transform child in parent)
+        {
+            child.parent = null;
+            children.Add(child);
+        }
+        parent.localScale = scale;
+        foreach (Transform child in children) child.parent = parent;
     }
 }
---
+++ Assets/Scripts/Scaleable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaleable : MonoBehaviour
+{
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.localScale = transform.localScale;
+    }
+}
